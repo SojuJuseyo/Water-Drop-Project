0bca8167ba7f624dc69b091694e32b08625203e5
Sarevok117 <sarevok117@hotmail.fr>
2016-04-07 13:16:55 +0900

    Commenting sound creation from local Varkiil dank songs

--- GameEngine/SoundSystem.cpp
+++ GameEngine/SoundSystem.cpp
@@ -27,13 +27,13 @@ void SoundSystem::createSound(SoundClass *pSound, const char* pFile)
 {
 	FMOD_RESULT res = m_pSystem->createSound(pFile, FMOD_DEFAULT, 0, pSound);
 	if (res)
-		printf("Create sound failed, error %i", res);
+		printf("Create sound failed, error %i\n", res);
 }
 
 void SoundSystem::initAllSounds()
 {
-	createSound(&soundMap["Home"], "C:\\Users\\Varkiil\\Desktop\\HOME - Resonance.mp3");
-	createSound(&soundMap["Alibi"], "C:\\Users\\Varkiil\\Desktop\\Love Alibi - K Klass Remix.mp3");
+//	createSound(&soundMap["Home"], "C:\\Users\\Varkiil\\Desktop\\HOME - Resonance.mp3");
+//	createSound(&soundMap["Alibi"], "C:\\Users\\Varkiil\\Desktop\\Love Alibi - K Klass Remix.mp3");
 }
 
 // Returns a channel FMOD, use it's method stop() to stop the sound
@@ -56,7 +56,7 @@ FMOD::Channel *SoundSystem::playSound(std::string soundName, bool bLoop = false)
 
 	FMOD_RESULT res = m_pSystem->playSound(pSound, 0, false, &channel);
 	if (res != FMOD_OK)
-		printf("Play sound failed, error %i", res);
+		printf("Play sound failed, error %i\n", res);
 	return channel;
 }
 
@@ -66,6 +66,6 @@ void SoundSystem::releaseSound(std::string soundName)
 	SoundClass pSound = soundMap[soundName];
 	FMOD_RESULT res = pSound->release();
 	if (res != FMOD_OK)
-		printf("Release sound failed, error %i", res);
+		printf("Release sound failed, error %i\n", res);
 
 }
